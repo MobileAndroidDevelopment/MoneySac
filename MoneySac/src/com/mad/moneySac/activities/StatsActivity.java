b00e97a6030c6c37c0205c79bb84226990ad7594
Sebastian <pib.sebastian.schultheiss@htw-saarland.de>
2014-02-19 16:59:06 +0100

    Statistik-Funktion hinzugefügt. Zum zweiten Mal. Noch nicht fertig.

---
+++ MoneySac/src/com/mad/moneySac/activities/StatsActivity.java
@@ -0,0 +1,91 @@
+package com.mad.moneySac.activities;
+
+import org.afree.chart.AFreeChart;
+import org.afree.chart.ChartFactory;
+import org.afree.chart.plot.PieLabelLinkStyle;
+import org.afree.chart.plot.PiePlot;
+import org.afree.data.general.DefaultPieDataset;
+import org.afree.data.general.PieDataset;
+import org.afree.graphics.geom.Font;
+import org.afree.ui.RectangleInsets;
+import org.afree.util.UnitType;
+
+import android.app.Activity;
+import android.graphics.Paint;
+import android.graphics.Typeface;
+import android.os.Bundle;
+
+import com.mad.moneySac.R;
+import com.mad.moneySac.model.SacEntry;
+import com.mad.moneySac.model.StatsView;
+
+public class StatsActivity extends Activity {
+	
+	private static final RectangleInsets ZERO_INSETS = new RectangleInsets(UnitType.ABSOLUTE, 0.0, 0.0, 0.0, 0.0);
+
+	AFreeChart	statsChart;
+	PiePlot		statsPlot;
+	StatsView	statsView;
+	double		income	= 0.0;
+    double		expense	= 0.0;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_stats);
+		
+		statsView = new StatsView(this);
+		statsChart = createPieChart(createDataset());
+		statsView.drawChart(statsChart);
+		
+        setContentView(statsView);
+        // requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+	private AFreeChart createPieChart(PieDataset dataset) {
+		AFreeChart chart = ChartFactory.createPieChart(	null,		// title
+														dataset,	// dataset
+														false,		// legend
+														false,		// tooltips
+														false );	// urls
+
+		chart.setNotify(false);
+		chart.setBorderStroke(0.0F);
+		chart.setPadding(ZERO_INSETS);
+		
+		statsPlot = (PiePlot) chart.getPlot();
+		statsPlot.setLabelFont(new Font("SansSerif", Typeface.NORMAL, 11));
+		statsPlot.setNoDataMessage("No data available");
+		statsPlot.setCircular(true);
+		statsPlot.setAutoPopulateSectionOutlinePaint(true);
+		statsPlot.setAutoPopulateSectionOutlineStroke(true);
+		statsPlot.setAutoPopulateSectionPaint(true);
+		statsPlot.setInteriorGap(0.0);
+		statsPlot.setLabelGap(0.0);
+		statsPlot.setLabelLinkMargin(0.0);
+		statsPlot.setLabelLinkStroke(0.2F);
+		statsPlot.setLabelLinkStyle(PieLabelLinkStyle.CUBIC_CURVE);
+		statsPlot.setLabelLinksVisible(true);
+		statsPlot.setLabelOutlineStroke(0.0F);
+		
+		return chart;
+	}
+	
+    private PieDataset createDataset() {
+        DefaultPieDataset dataset = new DefaultPieDataset();
+        
+        getExtrasFromBundle();
+        
+        dataset.setValue("Einnahmen", income);
+        dataset.setValue("Ausgaben", expense);
+        
+        return dataset;
+    }
+
+	private void getExtrasFromBundle() {
+		Bundle extras = getIntent().getExtras();
+		if (extras.containsKey(MoneySac.INCOME)) income = (Double) extras.get(MoneySac.INCOME);
+		if (extras.containsKey(MoneySac.EXPENSE)) expense = (Double) extras.get(MoneySac.EXPENSE);
+	}
+
+}
---
+++ MoneySac/src/com/mad/moneySac/model/StatsView.java
@@ -0,0 +1,52 @@
+package com.mad.moneySac.model;
+
+import org.afree.chart.AFreeChart;
+import org.afree.graphics.geom.RectShape;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class StatsView extends ImageView {
+	
+	Context context;
+	private Bitmap		bitmap;
+    private RectShape	rectArea;
+    private Canvas		canvas;
+    private AFreeChart	chart;
+
+    public StatsView(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+        this.context = context;
+    }
+
+    public StatsView(Context context) {
+        super(context);
+        this.context = context;
+        initChart();
+    }
+
+    private void initChart() {
+        int width = getResources().getConfiguration().smallestScreenWidthDp;
+        width = 480;
+        Toast.makeText(context, "size:"+width, Toast.LENGTH_SHORT).show();
+    	bitmap = Bitmap.createBitmap(width, width, Bitmap.Config.ARGB_8888);
+    	rectArea = new RectShape(0.0, 0.0, width, width);
+    }
+
+    public void drawChart(AFreeChart chart) {
+        canvas = new Canvas(bitmap);
+        this.chart = chart;             
+        this.chart.draw(canvas, rectArea);
+        setImageBitmap(bitmap);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);               
+    }
+    
+}
